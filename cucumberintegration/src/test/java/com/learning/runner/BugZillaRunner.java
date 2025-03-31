77b9782518bb3a0c39d3549ac02f48b8d3ee0f59
Rahul Singh Rathore <rahulrathore44@gmail.com>
2025-03-31 04:04:22 +0000

    Minor update to the code

--- cucumberintegration/src/test/java/com/learning/runner/BugZillaRunner.java
+++ cucumberintegration/src/test/java/com/learning/runner/BugZillaRunner.java
@@ -1,18 +1,3 @@
-/*
- * BugZillaRunner$
- *
- * Copyright (c) 2025  Pegasystems Inc.
- * All rights reserved.
- *
- * This  software  has  been  provided pursuant  to  a  License
- * Agreement  containing  restrictions on  its  use.   The  software
- * contains  valuable  trade secrets and proprietary information  of
- * Pegasystems Inc and is protected by  federal   copyright law.  It
- * may  not be copied,  modified,  translated or distributed in  any
- * form or medium,  disclosed to third parties or used in any manner
- * not provided for in  said  License Agreement except with  written
- * authorization from Pegasystems Inc.
- */
 package com.learning.runner;
 
 
